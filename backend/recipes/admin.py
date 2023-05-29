828bd0f0af051d9255b421fd36e3d71cb5e5ad34
Sergey Kraynev <sergejyit@gmail.com>
2023-05-30 00:27:35 +0400

    Add first arecipe app

---
+++ backend/recipes/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+
+from .models import Recipe, Ingredient, Tag, ShoppingCart, Favorite, Follow,\
+    MeasurementUnit
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    search_fields = ('name',)
+    list_filter = ('name', 'author', 'tags')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MeasurementUnit)
+class MeasureUnitAdmin(admin.ModelAdmin):
+    pass
---
+++ backend/recipes/models.py
@@ -0,0 +1,142 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
+from django.db import models
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        "Название",
+        max_length=200,
+        unique=True,
+    )
+    slug = models.SlugField(
+        "Краткое название",
+        max_length=200,
+        unique = True,
+    )
+    color = models.CharField(
+        "Цвет",
+        max_length=7,
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Бархатные Тяги'
+
+    def __str__(self):
+        return self.name
+
+
+class MeasurementUnit(models.Model):
+    unit = models.CharField('Единица измерения', max_length=200)
+
+
+class Ingredient(models.Model):
+    measurement_unit = models.ForeignKey(
+        MeasurementUnit,
+        related_name='ingredient',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField("Название", max_length=200)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+
+    author = models.ForeignKey(
+        User,
+        related_name='recipe',
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(
+        "Название",
+        max_length=200
+    )
+    image = models.ImageField(
+        "Картинка",
+        upload_to='images/',
+        null=True,
+        default=None,
+    )
+    text = models.TextField("Описание")
+    tags = models.ManyToManyField(
+        Tag,
+    )
+    cooking_time = models.TimeField("Время готовки")
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through="UsedIngredient"
+    )
+
+
+class UsedIngredient(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient,
+        related_name="used_ingredient",
+        on_delete=models.CASCADE,
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name="recipe",
+        on_delete=models.CASCADE,
+    )
+    amount = models.PositiveIntegerField(
+        "Количество",
+        validators=(MinValueValidator(1),),
+        error_messages={'validators': 'Количество не может быть меньше 1!'},
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        unique_together = ['user', 'author']
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+
+    def __str__(self):
+        return f'{self.user} follower of {self.author}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorite_user'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorite_recipe'
+    )
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping_user'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='shopping_recipe'
+    )
