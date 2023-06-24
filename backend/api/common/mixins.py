047d6f549fa56e0f688c35dd08df9a34d770cc87
Sergey Kraynev <sergejyit@gmail.com>
2023-06-24 16:18:55 +0400

    Update DenyPutMixin class creation

--- backend/api/common/mixins.py
+++ backend/api/common/mixins.py
@@ -1,7 +1,12 @@
-from rest_framework import status
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from django.db.models import prefetch_related_objects
+from rest_framework.mixins import (
+    CreateModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 
 
 class ListViewSet(GenericViewSet, ListModelMixin):
@@ -12,9 +17,34 @@ class ListRetrieveViewSet(GenericViewSet, RetrieveModelMixin, ListModelMixin):
     pass
 
 
-class DenyPutViewSet(ModelViewSet):
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.get('partial', False)
-        if not partial:
-            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return super().update(request, *args, **kwargs)
+class PatchModelMixin:
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        queryset = self.filter_queryset(self.get_queryset())
+        if queryset._prefetch_related_lookups:
+            instance._prefetched_objects_cache = {}
+            prefetch_related_objects([
+                instance], *queryset._prefetch_related_lookups
+            )
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+         serializer.save()
+
+
+class DenyPutViewSet(CreateModelMixin,
+                     ListModelMixin,
+                     RetrieveModelMixin,
+                     DestroyModelMixin,
+                     PatchModelMixin,
+                     GenericViewSet
+):
+    pass
+
