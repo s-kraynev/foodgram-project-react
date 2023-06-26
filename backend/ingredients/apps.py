6443d48eaa52b2a5bff0fa1519fa1ed57554a8d3
Sergey Kraynev <sergejyit@gmail.com>
2023-06-26 19:47:19 +0400

    Add plural names to apps

--- backend/ingredients/apps.py
+++ backend/ingredients/apps.py
@@ -5,3 +5,4 @@ class IngredientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ingredients'
     verbose_name = 'Ингредиенты'
+    verbose_name_plural = 'Ингредиенты'
--- backend/tags/apps.py
+++ backend/tags/apps.py
@@ -5,3 +5,4 @@ class TagsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tags'
     verbose_name = 'Теги'
+    verbose_name_plural = 'Теги'
